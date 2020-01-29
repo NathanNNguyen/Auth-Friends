164917f5d116a0734e9422ea4052f0efeb79d666
Nhan <natenguyen51294@gmail.com>
2020-01-28 16:56:23 -0800

    finished mvp

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 // import Loader from 'react-loader-spinner';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 import './styles.css';
+import AddFriend from './AddFriend';
 
 const FriendsList = () => {
   const [friends, setFriends] = useState([]);
@@ -18,16 +19,19 @@ const FriendsList = () => {
   }, []);
 
   return (
-    <div className='friends-list'>
-      {friends.map(friend => {
-        return (
-          <div key={friend.id} className='friends-styling'>
-            <h3>{friend.name}</h3>
-            <p>Age: {friend.age}</p>
-            <p>Email: {friend.email}</p>
-          </div>
-        )
-      })}
+    <div>
+      <AddFriend friends={friends} setFriends={setFriends}/>
+      <div className='friends-list'>
+        {friends.map(friend => {
+          return (
+            <div key={friend.id} className='friends-styling'>
+              <h3>{friend.name}</h3>
+              <p>Age: {friend.age}</p>
+              <p>Email: {friend.email}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
